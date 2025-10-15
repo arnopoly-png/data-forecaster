3b7dac7559973a2af23e08147231151e469b70b1
arnopoly-png <arnopoly@gmail.com>
2025-10-14 21:07:19 -0400

    Create label.tsx

---
+++ components/ui/label.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+
+export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
+return (
+<label className={"text-sm text-neutral-600 " + (className || "")} {...props} />
+);
+}
