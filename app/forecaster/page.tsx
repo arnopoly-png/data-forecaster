10f16f0f4c92b2835068d19d4350cc4e811dfe0a
arnopoly-png <arnopoly@gmail.com>
2025-10-14 21:36:38 -0400

    Update page.tsx

--- app/forecaster/page.tsx
+++ app/forecaster/page.tsx
@@ -1,4 +1,13 @@
-import KratosDataForecaster from "../../data-forecaster/components/KratosDataForecaster";
+import KratosDataForecaster from "../../components/KratosDataForecaster";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-background p-6">
+      <KratosDataForecaster />
+    </main>
+  );
+}
+import KratosDataForecaster from "../../components/KratosDataForecaster";
 
 export default function Page() {
   return (
